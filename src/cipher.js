7a79cd82d97935b6fe19821b8de81bf77700c590
Fiorela <fbasiliomanrique@gmail.com>
2019-06-10 11:24:45 -0500

    Avance del Cifrado y Descifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,50 @@
 
 window.cipher = {
-  encode: (number, letras) => {
-    
+  encode: (offset, string) => {  //Es independiente desligar responzabilidades
+
     /*Acá va tu código que cifra*/
     let cifrar="";
+    let ubicar=0;
 
-    for (let i = 0; i < letras.length; i++){
+    for (let i = 0; i < string.length; i++){    //El for recorre las letras al texto
 
-        if(parseInt(letras[i]) % i === 0)
+      if(string.charCodeAt(i) === 32) //Con el Codigo ASSCI para dar espacios 
+      {
+        ubicar = string.charCodeAt(i);
+      }
+      else{
+          ubicar=(string.toUpperCase().charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+      }
 
-        letras=prompt("Por favor ingrese un numero");
+      let cifrado = String.fromCharCode(ubicar);
 
-        let ubicar = (letras.toUpperCase().charCodeAt(i) - 65 + number) % 26 + 65;
-
-        let cifrado=String.fromCharCode(ubicar);
-        cifrar += cifrado;
+      cifrar += cifrado; //acumular letras
     }
     return cifrar;
   },
-
-  decode: (offset, string) => {
+  
+  decode: (offset, string) =>{ 
     /*Acá va tu código que descifra*/
+    let descifrado ="";
+    let ubicacionD = 0;
+    
+    for (let j = 0; j < string.length; j++){    //El for recorre las letras al texto
 
+      if(string.charCodeAt(j) === 32) // Con el Codigo ASSCI para dar espacios
+      {
+        ubicacionD = string.charCodeAt(j);
+      }
+      else{
+          ubicacionD=(string.toUpperCase().charCodeAt(j) + 65 - parseInt(offset)) % 26 + 65;
+      }
+
+      let palabraDescifrada = String.fromCharCode(ubicacionD);
+
+      descifrado += palabraDescifrada; //acumular letras
+
+    }
+  
+   return descifrado;
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ let mensaje = document.getElementById("incorrecto");
 let contar = 0;
 const final = 3;
 
+const elemento = document.getElementById("Bienvenida");
+const pagdos = document.getElementById("formulario");
+pagdos.classList.add("hide");
 
-clave.addEventListener("click",function(){
+clave.addEventListener("click", () => {
  const nombre= document.getElementById("contraseña").value;
 
     if(nombre === "LABORATORIA"){
-        document.getElementById("Bienvenida").style.display="none";
-        document.getElementById("formulario").style.display="block";
+       
+        elemento.classList.add("hide");
+        pagdos.classList.remove("hide");
     
-        alertify.alert("Contraseña Correcta")
     }
     if(nombre !== "LABORATORIA"){
         contar++;
@@ -26,6 +29,8 @@ clave.addEventListener("click",function(){
         }
         else{
             mensaje.innerHTML="Ya utilizastes todos tus intentos";
+            contraseña.disabled=true;
+            
             
         }   
     }
@@ -34,15 +39,49 @@ else{
 }
 });
 
+/*Cifrando*/
 const cifra = document.getElementById("cifrando");
 cifra.addEventListener('click',() => {
 
-    let number = document.getElementById('letras').value;
-    let letras = document.getElementById('number').value;
+    let letras = document.getElementById('letras');
+    let number = document.getElementById('number');
     let result = document.getElementById('result');
-    debugger;
-    let respuestas=cipher.encode(number, letras);
+    debugger; //Comprobar cuando vas ejecutando
+    let respuestas=cipher.encode(number.value, letras.value);
     result.innerHTML=(respuestas);
 })
 
+/*Descifrando*/
+const descifra = document.getElementById("descifrando");
+descifra.addEventListener('click',()=>{
+    
+    let letras = document.getElementById('letras');
+    let number = document.getElementById('number');
+    let result = document.getElementById('result');
+    debugger;
+    let respuest=cipher.decode(number.value, letras.value);
+    result.innerHTML=(respuest);
+})
+
+/*Limpiar*/
+const limpia = document.getElementById("limpiar");
+limpia.addEventListener("click",()=>{
+    document.getElementById("number").value="";
+    document.getElementById("letras").value="";
+    document.getElementById("result").value="";
+
+ 
+})
+
+/*Salir*/
+
+const casa = document.getElementById("salir");
+casa.addEventListener("click", ()=>{
+
+    document.getElementById("formulario").style.display = "none";    
+    document.getElementById("Bienvenida").style.display = "block";
+    contraseña.value="";
+
+})
+   
   
